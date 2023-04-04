f67d76f8427186f795e29dd281ceab4746b3f1ca
Дмитрий Мазин <mitya.mazin@mail.ru>
2023-04-04 09:08:41 +0300

    Add project files.

---
+++ Traning/Program.cs
@@ -0,0 +1,20 @@
+﻿try
+{
+    int[] numbers = new int[4];
+    numbers[7] = 9;     // IndexOutOfRangeException
+
+    int x = 5;
+    int y = x / 0;  // DivideByZeroException
+    Console.WriteLine($"Результат: {y}");
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("Возникло исключение DivideByZeroException");
+}
+catch (IndexOutOfRangeException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+Console.Read();
+
